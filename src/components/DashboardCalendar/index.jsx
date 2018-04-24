a6dbfa59bcf1e9cfa06acfa099282be6ac83a2cc
Francesco Angi <fangi@intesys.it>
2018-04-24 17:26:46 +0200

    dashboard calendar: today paper

--- src/components/DashboardCalendar/index.jsx
+++ src/components/DashboardCalendar/index.jsx
@@ -5,13 +5,16 @@ import { withStyles } from 'material-ui/styles';
 import Calendar from 'material-ui-pickers/DatePicker/Calendar';
 import moment from 'moment';
 import { Grid, Paper, Typography } from 'material-ui';
+import KeyboardArrowLeft from '@material-ui/icons/es/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/es/KeyboardArrowRight';
 
 const styles = (theme) => {
   const t = theme;
   return ({
     today: {
       textAlign: 'center',
-      // backgroundColor: theme.palette.primary.light,
+      height: '100%',
+      backgroundColor: theme.palette.secondary.dark,
     },
     calendar: {
       // backgroundColor: theme.palette.secondary.dark,
@@ -22,13 +25,17 @@ const styles = (theme) => {
 const DashboardCalendar = props => (
   <DashboardSection title="Calendar">
     <Grid container justify="space-between" spacing={40}>
-      <Grid item xs={5} className={props.classes.today}>
-        <Typography variant="title">TODAY</Typography>
-        <br />
-        <Typography variant="title">MAY</Typography>
-        <Typography variant="title">18</Typography>
-        <br />
-        <Typography variant="title">2016</Typography>
+      <Grid item xs={5}>
+        <Paper elevation={0} className={props.classes.today}>
+          <br /><br /><br />
+          <Typography variant="display1">TODAY</Typography>
+          <br />
+          <Typography variant="title">MAY</Typography>
+          <Typography variant="title">18</Typography>
+          <Typography variant="title">Friday</Typography>
+          <br /> <br />
+          <Typography variant="title">2018</Typography>
+        </Paper>
       </Grid>
       <Grid item xs={7} className={props.classes.calendar}>
         <Calendar
@@ -37,6 +44,8 @@ const DashboardCalendar = props => (
           cancelLabel={false}
           firstDayOfWeek={1}
           mode="portrait"
+          leftArrowIcon={<KeyboardArrowLeft />}
+          rightArrowIcon={<KeyboardArrowRight />}
           open
         />
       </Grid>
