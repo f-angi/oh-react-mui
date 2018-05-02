0f547b2611fc788b85f10ec552c056271077e888
Francesco Angi <fangi@intesys.it>
2018-05-02 18:18:52 +0200

    dashboard event

--- src/components/DashboardAppointments/DashboardAppointment/index.jsx
+++ src/components/DashboardAppointments/DashboardAppointment/index.jsx
@@ -3,30 +3,55 @@ import { withStyles } from 'material-ui/styles';
 import { Checkbox, Grid, Paper, Typography } from 'material-ui';
 
 const styles = theme => ({
-  root: theme.mixins.gutters({
+  appointmentDone: theme.mixins.gutters({
     paddingTop: 4,
     paddingBottom: 4,
     marginBottom: theme.spacing.unit * 2,
     textAlign: 'center',
+    backgroundColor: theme.palette.secondary.light,
+  }),
+  appointmentIncoming: theme.mixins.gutters({
+    paddingTop: 4,
+    paddingBottom: 4,
+    marginBottom: theme.spacing.unit * 2,
+    textAlign: 'center',
+    backgroundColor: theme.palette.primary.light,
   }),
 });
 
-const DashboardAppointment = (props) => {
-  const { time, subject, done } = props.appointment;
-  return (
-    <Paper className={props.classes.root} elevation={1}>
-      <Grid container direction="row" justify="space-between" alignItems="center">
-        <Grid item xs={3}>
-          <Typography variant="body1">{time}</Typography>
+class DashboardAppointment extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      checked: this.props.appointment.done,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange() {
+    this.setState({checked: !this.state.checked});
+  }
+
+  render() {
+    const { time, subject } = this.props.appointment;
+    const className = this.state.checked ? this.props.classes.appointmentDone : this.props.classes.appointmentIncoming;
+    return (
+      <Paper className={className} elevation={1}>
+        <Grid container direction="row" justify="space-between" alignItems="center">
+          <Grid item xs={3}>
+            <Typography variant="body1">{time}</Typography>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="body1">{subject}</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Checkbox checked={this.state.checked} onChange={this.handleChange} />
+          </Grid>
         </Grid>
-        <Grid item xs={6}>
-          <Typography variant="body1">{subject}</Typography>
-        </Grid>
-        <Grid item xs={3}>
-          <Checkbox checked={done} />
-        </Grid>
-      </Grid>
-    </Paper>);
-};
+      </Paper>);
+  }
+}
 
 export default withStyles(styles)(DashboardAppointment);
