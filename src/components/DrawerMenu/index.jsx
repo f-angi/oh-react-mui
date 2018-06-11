c7ddd2e0fcbf0844637f88f599690c13f148a92a
marconi3 <federicoc.2000@gmail.com>
2018-06-11 10:33:17 +0200

    Change hospital name popup dialog and drawerMenu width fix
    Drawer fix on some devices with low resolution

--- src/components/DrawerMenu/index.jsx
+++ src/components/DrawerMenu/index.jsx
@@ -1,70 +1,71 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
 
-import {withStyles} from '@material-ui/core/styles';
-import {Badge, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
-import DrawerSearch from './DrawerSearch';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import ChatIcon from '@material-ui/icons/Chat';
 import EventNoteIcon from '@material-ui/icons/EventNote';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { Badge, Divider, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+
+import DrawerSearch from './DrawerSearch';
 
 
 const styles = theme => ({
-    drawerList: {
-        width: 400,
-    },
+  drawerList: {
+    width: '95%',
+  },
 });
 
 const DrawerMenu = props => (
-    <Drawer anchor="right" open={props.open} onClose={props.toggleDrawer}>
-        <div className={props.classes.drawerList}>
-            <List>
-                <div>
-                    {props.links.map(link => (
-                        <ListItem button key={link}>
-                            <ListItemText primary={link}/>
-                        </ListItem>))}
-                </div>
-            </List>
-            <Divider/>
-            <List>
-                <div>
-                    <ListItem button>
-                        <ListItemIcon>
-                            <Badge color="primary" badgeContent={4}><NotificationsIcon/></Badge>
-                        </ListItemIcon>
-                        <ListItemText primary="Notifications"/>
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemIcon>
-                            <Badge color="primary" badgeContent={8}><ChatIcon/></Badge>
-                        </ListItemIcon>
-                        <ListItemText primary="Messages"/>
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemIcon>
-                            <Badge color="primary" badgeContent={8}><EventNoteIcon/></Badge>
-                        </ListItemIcon>
-                        <ListItemText primary="Calendar"/>
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemIcon>
-                            <Badge color="primary" badgeContent={8}><SettingsIcon/></Badge>
-                        </ListItemIcon>
-                        <ListItemText primary="Settings"/>
-                    </ListItem>
-                </div>
-            </List>
-            <Divider/>
-            <List>
-                <div>
-                    <ListItem button>
-                        <DrawerSearch/>
-                    </ListItem>
-                </div>
-            </List>
+  <Drawer anchor="right" open={props.open} onClose={props.toggleDrawer}>
+    <div className={props.classes.drawerList}>
+      <List>
+        <div>
+          {props.links.map(link => (
+            <ListItem button key={link}>
+              <ListItemText primary={link} />
+            </ListItem>))}
         </div>
-    </Drawer>
+      </List>
+      <Divider />
+      <List>
+        <div>
+          <ListItem button>
+            <ListItemIcon>
+              <Badge color="primary" badgeContent={4}><NotificationsIcon /></Badge>
+            </ListItemIcon>
+            <ListItemText primary="Notifications" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <Badge color="primary" badgeContent={8}><ChatIcon /></Badge>
+            </ListItemIcon>
+            <ListItemText primary="Messages" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <Badge color="primary" badgeContent={8}><EventNoteIcon /></Badge>
+            </ListItemIcon>
+            <ListItemText primary="Calendar" />
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <Badge color="primary" badgeContent={8}><SettingsIcon /></Badge>
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItem>
+        </div>
+      </List>
+      <Divider />
+      <List>
+        <div>
+          <ListItem button>
+            <DrawerSearch />
+          </ListItem>
+        </div>
+      </List>
+    </div>
+  </Drawer>
 );
 
 export default withStyles(styles)(DrawerMenu);
